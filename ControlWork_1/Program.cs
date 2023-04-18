59e0361f2e83c1ecefe306e07222166f2fdec98c
Elisabeth <m.e.elisa@yandex.ru>
2023-04-18 22:10:48 +0300

    Решение задачи

---
+++ ControlWork_1/Program.cs
@@ -0,0 +1,48 @@
+﻿string[] newArray = new string[5];
+
+void ArrayInput(string[] newArray)
+{
+    for (int i = 0; i < newArray.Length; i++)
+    {
+        Console.WriteLine($"Введите {i + 1} элемент массива");
+        newArray[i] = Console.ReadLine()!;
+    }
+}
+
+
+string[] Array(string[] newArray)
+{
+    int n = 0;
+    for (int i = 0; i < newArray.Length; i++)
+    {
+        if (newArray[i].Length <= 3)
+        {
+            n++;
+        }
+    }
+
+    string[] Result = new string[n];
+
+    int m = 0;
+    for (int i = 0; i < newArray.Length; i++)
+    {
+        if (newArray[i].Length <= 3)
+        {
+            Result[m] = newArray[i];
+            m++;
+        }
+    }
+    return Result;
+}
+
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        System.Console.WriteLine("[" + string.Join(", ", array) + "]");
+    }
+
+}
+ArrayInput(newArray);
+System.Console.WriteLine();
+PrintArray(Array(newArray));
